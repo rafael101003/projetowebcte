6704be1391380355a42ce22298a883b9bc849d72
SeijiDev <canalotakurafael2003@gmail.com>
2026-05-28 03:00:34 -0300

    feat: adicionado efeito paralax na home, melhorado responsividade com telas de celular, adicionado menu sanduiche nos dispositivos móveis

--- js/script.js
+++ js/script.js
@@ -7,4 +7,13 @@ function toggleCard(card) {
     });
 
     card.classList.toggle("ativo");
-}
+};
+
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  menuToggle.addEventListener('click', () => {
+    // Adiciona ou remove a classe que mostra o menu
+    navLinks.classList.toggle('active');
+  });
+
